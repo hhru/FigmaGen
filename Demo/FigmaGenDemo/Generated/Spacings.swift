ece4a0dd841761ff6e796735a38243e1ce9992da
Roman Fedyanin <r.fedyanin@hh.ru>
2020-08-07 13:08:11 +0300

    Improved demo template for spacings

--- Demo/FigmaGenDemo/Generated/Spacings.swift
+++ Demo/FigmaGenDemo/Generated/Spacings.swift
@@ -1,46 +1,56 @@
 // swiftlint:disable all
-import Foundation
+import UIKit
 
-public enum Spacings {
+public protocol Spacing {
+    init(_ value: Double)
+}
+
+extension Int: Spacing { }
+extension UInt: Spacing { }
+extension Float: Spacing { }
+extension Double: Spacing { }
+extension CGFloat: Spacing { }
+
+extension Spacing {
 
     /// L
     ///
     /// Value: 24.0.
-    public static let L: CGFloat = 24.0
+    public static var l: Self { Self(24.0) }
 
     /// M
     ///
     /// Value: 16.0.
-    public static let M: CGFloat = 16.0
+    public static var m: Self { Self(16.0) }
 
     /// MPlus
     ///
     /// Value: 20.0.
-    public static let MPlus: CGFloat = 20.0
+    public static var mplus: Self { Self(20.0) }
 
     /// S
     ///
     /// Value: 12.0.
-    public static let S: CGFloat = 12.0
+    public static var s: Self { Self(12.0) }
 
     /// XL
     ///
     /// Value: 32.0.
-    public static let XL: CGFloat = 32.0
+    public static var xl: Self { Self(32.0) }
 
     /// XS
     ///
     /// Value: 8.0.
-    public static let XS: CGFloat = 8.0
+    public static var xs: Self { Self(8.0) }
 
     /// XXS
     ///
     /// Value: 4.0.
-    public static let XXS: CGFloat = 4.0
+    public static var xxs: Self { Self(4.0) }
 
     /// XXXS
     ///
     /// Value: 1.0.
-    public static let XXXS: CGFloat = 1.0
+    public static var xxxs: Self { Self(1.0) }
 }
 // swiftlint:enable all
