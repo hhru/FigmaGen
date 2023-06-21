a16f4ae22696aeaa595c29492b57738c6b5b17b8
Timur Shafigullin <hometim55@gmail.com>
2023-06-21 19:54:42 +0300

    Подключение SwiftLint через Mint

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "FigmaGen",
     platforms: [
-       .macOS(.v10_12)
+        .macOS(.v12)
     ],
     products: [
         .executable(name: "figmagen", targets: ["FigmaGen"]),
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.3"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.6"),
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
         .package(url: "https://github.com/kylef/Stencil.git", from: "0.13.0"),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.7.2"),
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/almazrafi/DictionaryCoder.git", from: "1.0.0")
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "FigmaGen",
             dependencies: [
                 "Yams",
--- Scripts/swiftlint.sh
+++ Scripts/swiftlint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [[ ! $CI && ! $SKIP_SWIFTLINT ]]; then
-  if which swiftlint >/dev/null; then
-    swiftlint --no-cache
-  else
-    echo "error: SwiftLint does not exist, download it from https://github.com/realm/SwiftLint"
-    exit 1
-  fi
+if [[ "${SKIP_SWIFTLINT}" == "YES" ]]; then
+  exit 0
 fi
+
+readonly helpers_path="$( cd "$( dirname "$0" )" && pwd )/Helpers"
+
+source "${helpers_path}/script-run.sh"
+run swiftlint --quiet || true
