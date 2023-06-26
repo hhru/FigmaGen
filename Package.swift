bec18a1ad9b403ceeaa690300e57dcfc2df04311
Timur Shafigullin <hometim55@gmail.com>
2023-06-26 13:25:03 +0300

    Обновлены зависимости

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.3"),
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0"),
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.6"),
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
         .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.13.0"),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.7.2"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.0"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "8.0.0"),
         .package(url: "https://github.com/almazrafi/DictionaryCoder.git", from: "1.0.0")
     ],
     targets: [
