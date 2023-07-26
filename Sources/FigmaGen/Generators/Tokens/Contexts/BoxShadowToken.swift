18769d72cac8120949eafabfb966a7e3aa250990
Timur Shafigullin <hometim55@gmail.com>
2023-07-26 13:32:49 +0300

    Темы для теней

--- Sources/FigmaGen/Generators/Tokens/Contexts/BoxShadowToken.swift
+++ Sources/FigmaGen/Generators/Tokens/Contexts/BoxShadowToken.swift
@@ -2,13 +2,23 @@ import Foundation
 
 struct BoxShadowToken {
 
+    // MARK: - Nested Types
+
+    struct Theme {
+
+        // MARK: - Instance Properties
+
+        let color: String
+        let type: String
+        let x: String
+        let y: String
+        let blur: String
+        let spread: String
+    }
+
     // MARK: - Instance Properties
 
     let path: [String]
-    let color: String
-    let type: String
-    let x: String
-    let y: String
-    let blur: String
-    let spread: String
+    let dayTheme: Theme
+    let nightTheme: Theme
 }
--- Sources/FigmaGen/Generators/Tokens/Generators/BoxShadow/DefaultBoxShadowTokensGenerator.swift
+++ Sources/FigmaGen/Generators/Tokens/Generators/BoxShadow/DefaultBoxShadowTokensGenerator.swift
@@ -16,13 +16,8 @@ final class DefaultBoxShadowTokensGenerator: BoxShadowTokensGenerator {
 
     // MARK: - Instance Methods
 
-    private func makeBoxShadowToken(from tokenValue: TokenValue, tokenValues: TokenValues) throws -> BoxShadowToken? {
-        guard case let .boxShadow(value) = tokenValue.type else {
-            return nil
-        }
-
-        return BoxShadowToken(
-            path: tokenValue.name.components(separatedBy: "."),
+    private func makeTheme(value: TokenBoxShadowValue) -> BoxShadowToken.Theme {
+        BoxShadowToken.Theme(
             color: value.color,
             type: value.type,
             x: value.x,
@@ -32,10 +27,35 @@ final class DefaultBoxShadowTokensGenerator: BoxShadowTokensGenerator {
         )
     }
 
+    private func makeBoxShadowToken(
+        from dayTokenValue: TokenValue,
+        tokenValues: TokenValues
+    ) throws -> BoxShadowToken? {
+        guard case let .boxShadow(dayValue) = dayTokenValue.type else {
+            return nil
+        }
+
+        guard let nightTokenValue = tokenValues.night.first(where: { $0.name == dayTokenValue.name }) else {
+            throw BoxShadowTokensGeneratorError(code: .nightValueNotFound(tokenName: dayTokenValue.name))
+        }
+
+        guard case let .boxShadow(nightValue) = nightTokenValue.type else {
+            throw BoxShadowTokensGeneratorError(code: .nightValueNotFound(tokenName: dayTokenValue.name))
+        }
+
+        return BoxShadowToken(
+            path: dayTokenValue.name.components(separatedBy: "."),
+            dayTheme: makeTheme(value: dayValue),
+            nightTheme: makeTheme(value: nightValue)
+        )
+    }
+
     // MARK: -
 
     func generate(renderParameters: RenderParameters, tokenValues: TokenValues) throws {
-        let boxShadows = try tokenValues.day.compactMap { try makeBoxShadowToken(from: $0, tokenValues: tokenValues) }
+        let boxShadows = try tokenValues.day
+            .compactMap { try makeBoxShadowToken(from: $0, tokenValues: tokenValues) }
+            .sorted { $0.path.joined() < $1.path.joined() }
 
         try templateRenderer.renderTemplate(
             renderParameters.template,
