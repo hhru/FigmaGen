251c8a5633a51dbdd75cf1de99b2ac98f497a00a
Darya <cleverdevil@yandex.ru>
2024-05-17 14:04:25 +0300

    reset first if

--- Sources/FigmaGen/Resolvers/AccessToken/DefaultAccessTokenResolver.swift
+++ Sources/FigmaGen/Resolvers/AccessToken/DefaultAccessTokenResolver.swift
@@ -1,7 +1,5 @@
 import Foundation
-#if !os(Linux)
 import KeychainAccess
-#endif
 
 final class DefaultAccessTokenResolver: AccessTokenResolver {
 
@@ -11,13 +9,9 @@ final class DefaultAccessTokenResolver: AccessTokenResolver {
         } else if let environmentVariable = configuration?.environmentVariable,
                   let accessToken = ProcessInfo.processInfo.environment[environmentVariable] {
             return accessToken
-        } else if let parameters = configuration?.keychainParameters {
-        #if !os(Linux)
-            let accessToken = try? Keychain(service: parameters.service).getString(parameters.key)
+        } else if let parameters = configuration?.keychainParameters,
+                  let accessToken = try? Keychain(service: parameters.service).getString(parameters.key) {
             return accessToken
-        #else
-            return nil
-        #endif
         } else {
             return nil
         }
