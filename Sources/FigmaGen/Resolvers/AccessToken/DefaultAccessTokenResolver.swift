d93209013f0cab88eb65fe35d14b2bee0659676e
Darya <cleverdevil@yandex.ru>
2024-05-17 14:09:25 +0300

    Исправление джобы для Linux

--- Sources/FigmaGen/Resolvers/AccessToken/DefaultAccessTokenResolver.swift
+++ Sources/FigmaGen/Resolvers/AccessToken/DefaultAccessTokenResolver.swift
@@ -1,5 +1,7 @@
 import Foundation
+#if os(macOS)
 import KeychainAccess
+#endif
 
 final class DefaultAccessTokenResolver: AccessTokenResolver {
 
@@ -9,9 +11,13 @@ final class DefaultAccessTokenResolver: AccessTokenResolver {
         } else if let environmentVariable = configuration?.environmentVariable,
                   let accessToken = ProcessInfo.processInfo.environment[environmentVariable] {
             return accessToken
-        } else if let parameters = configuration?.keychainParameters,
-                  let accessToken = try? Keychain(service: parameters.service).getString(parameters.key) {
+        } else if let parameters = configuration?.keychainParameters {
+        #if os(macOS)
+            let accessToken = try? Keychain(service: parameters.service).getString(parameters.key)
             return accessToken
+        #else
+            return nil
+        #endif
         } else {
             return nil
         }
