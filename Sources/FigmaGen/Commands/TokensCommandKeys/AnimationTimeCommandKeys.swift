f5db52445a651512d93247aaa01b5c66c03fa824
Darya Viter <cleverdevil@yandex.ru>
2025-07-02 14:33:46 +0300

    Правки по комментариям

--- Sources/FigmaGen/Commands/TokensCommandKeys/AnimationTimeCommandKeys.swift
+++ Sources/FigmaGen/Commands/TokensCommandKeys/AnimationTimeCommandKeys.swift
@@ -5,9 +5,9 @@ enum AnimationTimeCommandKeys {
     static let template = Key<String>(
         "--animationTime-template",
         description: """
-        Path to the template file.
-        If no template is passed a default template will be used.
-        """
+            Path to the template file.
+            If no template is passed a default template will be used.
+            """
     )
 
     static let templateOptions = VariadicKey<String>(
--- Sources/FigmaGen/Commands/TokensCommandKeys/BordersCommandKeys.swift
+++ Sources/FigmaGen/Commands/TokensCommandKeys/BordersCommandKeys.swift
@@ -5,9 +5,9 @@ enum BordersCommandKeys {
     static let template = Key<String>(
         "--borders-template",
         description: """
-        Path to the template file.
-        If no template is passed a default template will be used.
-        """
+            Path to the template file.
+            If no template is passed a default template will be used.
+            """
     )
 
     static let templateOptions = VariadicKey<String>(
